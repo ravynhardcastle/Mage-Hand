e0121e54815e5c4bf5917122328fe28e37d61d67
ravynhardcastle <16183076+ravynhardcastle@users.noreply.github.com>
2026-04-04 12:27:21 -0600

    slightly more verbose message lol

--- scripts/server.py
+++ scripts/server.py
@@ -119,7 +119,7 @@ async def websocket_endpoint(websocket: WebSocket):
                 )
 
     except WebSocketDisconnect:
-        logger.info("Client disconnected")
+        logger.info("Client disconnected, saving prematurely")
         model_name = f"model_{time_start}_turns{max_turns}_runs{num_runs}.pth"
         save_path = model_dir / model_name
         model.save_model(save_path)
